c907c5880b2fb96bf1129322cce9b9b14f7fc943
André Miranda <aegis.andre@gmail.com>
2024-12-05 17:22:01 -0300

    :sparkles: feat: add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from './components/ui/button/button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'angular-ecommerce';
-}
+export class AppComponent {}
